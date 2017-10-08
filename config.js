41cc30cf1ca872f7e685bd5e281d23e571c8db80
Sambhav Jain <sambhavjain2612@gmail.com>
2017-10-08 19:10:05 +0530

    not working twitter bot

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+module.exports = {
+  consumer_key: 'RunDOh9R9qDI8dGJ5sFk3usQC',
+  consumer_secret: '3Q5SwgCHMdx9m2gCPWm9sMzyOGote7Obw755AU4tuWH3e3wo6e',
+  access_token_key: '725055718848606212-GaMWCOBGkDRUjiW2AVhgOIK47bgujZT',
+  access_token_secret: 'TtSM8H79ue0cuPkEQC75VQuVaeRMHWEA7dEoI2F3pQLbt'
+}
